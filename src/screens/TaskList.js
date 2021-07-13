321b70138c2e433dfa4e3ab4e155ae1117b675cc
pheliperost <phelipe_rost@hotmail.com>
2021-07-13 01:40:56 -0300

    adding background and setting flex

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,ImageBackground, StyleSheet } from 'react-native'
+import ImageToday from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component{
     render(){
         return(
-            <View>
-                <Text>Tasklist</Text>
+            <View style={styles.container}>
+                <ImageBackground source={ImageToday}
+                style={styles.background}>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>Tasklist</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1 
+    },
+    background: {
+        flex: 3
+    },
+    taskList:{
+        flex: 7
+    }
+});
