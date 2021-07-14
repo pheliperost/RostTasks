44b2d632a9ad3ee0002e3bfa054704fd01a84a5e
pheliperost <phelipe_rost@hotmail.com>
2021-07-14 00:42:27 -0300

    formatação de data e estilo de textos.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { View,Text, StyleSheet} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import CommonStyles from '../commonStyles'
+import commonStyles from '../commonStyles'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 
 export default props =>{
+    
+    const doneOrNotStyle = props.doneAt != null ? 
+    {textDecorationLine: 'line-through'} : {}
+
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br')
+        .format('ddd, D [de] MMMM')
+
     return(
         <View style={styles.container}>
             <View style={styles.checkContainer}>
                 {getCheckView(props.doneAt)}
             </View>
             <View>
-                <Text>{props.desc}</Text>
-                <Text>{props.estimateAt + ""}</Text>
+                <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+                <Text style={styles.date}>{formattedDate}</Text>
             </View>
             
         </View>
@@ -62,6 +73,16 @@ const styles = StyleSheet.create({
             backgroundColor: '#4D7031',
             alignItems: 'center',
             justifyContent: 'center'
+        },
+        desc:{
+            fontFamily: commonStyles.fontFamily,
+            color: commonStyles.colors.mainText,
+            fontSize: 15
+        },
+        date:{
+            fontFamily: commonStyles.fontFamily,
+            color: commonStyles.colors.subText,
+            fontSize: 12
         }
 
 })
