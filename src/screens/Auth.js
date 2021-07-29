23cceb30f2dcd5d36a2193f726b2f488093c0ad1
pheliperost <phelipe_rost@hotmail.com>
2021-07-29 01:09:44 -0300

    ternario entre novo e antigo usu'ario

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -12,8 +12,11 @@ import commonStyles from '../commonStyles'
 
 export default class Auth extends Component {
     state = {
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: '',
+        stageNew: true
     }
 
     render(){
@@ -22,17 +25,34 @@ export default class Auth extends Component {
                 style={styles.background}>
                 <Text style={styles.title}>Task</Text>
                 <View style={styles.formContainer}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Crie a sua conta':'Informe seus dados'}
+                    </Text>
+                    {this.state.stageNew && 
+                        <TextInput placeholder='Nome' value={this.state.name}
+                            style={styles.input} 
+                            onChangeText={name => this.setState({name})}/>
+                    }
                     <TextInput placeholder='E-mail' value={this.state.email}
                        style={styles.input} 
                        onChangeText={email => this.setState({email})}/>
                        
                     <TextInput placeholder='Senha' value={this.state.password}
+                        secureTextEntry={true}
                        style={styles.input} 
                        onChangeText={password => this.setState({password})}/>
 
+                    {this.state.stageNew && 
+                        <TextInput placeholder='Confirmar Senha' value={this.state.confirmPassword}
+                            secureTextEntry={true}
+                            style={styles.input} 
+                            onChangeText={confirmPassword => this.setState({confirmPassword})}/>
+                    }
                     <TouchableOpacity>
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>
+                            {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -53,6 +73,13 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secundary,
         fontSize: 70,
+        marginBottom: 10
+    },
+    subtitle:{
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
         marginBottom: 10
     },
     formContainer:{
