e3077fb32cafe7bf5f92d4c6cd6ba2e10df8b127
pheliperost <phelipe_rost@hotmail.com>
2021-08-02 21:19:58 -0300

    color by drawer option

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -2,6 +2,9 @@ export default {
     fontFamily: 'Lato',
     colors:{
         today: '#b13b44',
+        tomorrow: '#C9742E',
+        week: '#15721E',
+        month: '#1631BE',
         secundary: '#FFF',
         mainText: '#222',
         subText: '#555'
