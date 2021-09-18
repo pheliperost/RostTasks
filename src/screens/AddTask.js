89ceeca9fc9281ee54b33e1deb6daed096cf22c3
pheliperost <phelipe_rost@hotmail.com>
2021-09-18 15:51:26 -0300

    dropdown trazendo os valores do tipo de evento

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -4,6 +4,10 @@ import commonStyles from '../commonStyles'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import moment from 'moment'
 import SelectDropdown from 'react-native-select-dropdown'
+import ModalSelector from 'react-native-modal-selector-searchable'
+
+import { server, showError } from '../common'
+import axios from 'axios'
 
 const initialState = { desc: '', date: new Date(), showDatePicker: false}
 
@@ -50,7 +54,22 @@ export default class AddTask extends Component{
             return datePicker
     }
 
+
+    loadEventTypeItems = async () => {
+        try{
+            const res = await axios.get(`${server}/eventtype`)
+
+          
+            this.setState({eventtype: res.data})
+            //console.log(res.data.body)
+            
+        }catch(e){
+            showError(e)
+        }
+    }
+
     render(){
+       this.loadEventTypeItems()
         return(
             <Modal transparent={true}
                 visible={this.props.isVisible}
@@ -64,26 +83,17 @@ export default class AddTask extends Component{
 
                 <View style={styles.container}>
                     <Text style={styles.header}>Nova Tarefa</Text>
+
+
                     <TextInput style={styles.input}
                         placeholder='informe a descrição...' 
                         onChangeText={desc => this.setState({desc})}
                         value={this.state.desc}/>
 
-                        <SelectDropdown
-                            data={countries}
-                            onSelect={(selectedItem, index) => {
-                                console.log(selectedItem, index)
-                            }}
-                            buttonTextAfterSelection={(selectedItem, index) => {
-                                // text represented after item is selected
-                                // if data array is an array of objects then return selectedItem.property to render after item is selected
-                                return selectedItem
-                            }}
-                            rowTextForSelection={(item, index) => {
-                                // text represented for each item in dropdown
-                                // if data array is an array of objects then return item.property to represent item in dropdown
-                                return item
-                            }}
+                        <ModalSelector
+                            data={this.state.eventtype}
+                            keyExtractor= {item => item.id}
+                            labelExtractor= {item => item.type}
                         />
 
 
